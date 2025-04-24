f78ea59fc68c7802a64b067e5343a7f656b2eb3b
litian <litian33@gmail.com>
2025-04-24 15:30:12 +0800

    修改测试配置

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,51 +8,44 @@ const { networks } = require("./networks");
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  solidity: "0.8.9",
   compilers: [
     {
-      version: "0.8.28",
-      settings: { 
-        evmVersion: "cancun",
-        optimizer: { enabled: true, runs: 200 } 
-      }
-    },
-    {
-      version: "0.8.23",
-      settings: { 
-        evmVersion: "shanghai",
-        optimizer: { enabled: true, runs: 200 } 
-      }
-    },
-    {
-      version: "0.8.19",
-      settings: { 
+      version: "0.8.9",
+      settings: {
         evmVersion: "london",
-        optimizer: { enabled: true, runs: 200 } 
+        optimizer: { enabled: true, runs: 200 }
       }
     },
-    {
-      version: "0.8.17",
-      settings: { 
-        evmVersion: "london",
-        optimizer: { enabled: true, runs: 200 } 
-      }
-    },
-    {
-      version: "0.8.6",
-      settings: { 
-        evmVersion: "berlin",
-        optimizer: { enabled: true, runs: 200 } 
-      }
-    },
-    {
-      version: "0.7.6",
-      settings: { 
-        evmVersion: "istanbul",
-        optimizer: { enabled: true, runs: 200 } 
-      }
-    }
   ],
+  defaultNetwork: "dev",
   networks: {
-    ...networks,
+    dev: {
+      url: "http://localhost:8545",
+      accounts: process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : [],
+      gasPrice: 1200000000,
+      hardfork: "london"
+    },
+    juSolo: {
+      url: "http://192.168.8.18:8545",
+      accounts: process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : [],
+      gasPrice: 1100000000
+    },
+    juLocal: {
+      url: "http://192.168.8.14:8545",
+      accounts: process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : [],
+      gasPrice: 1100000000,
+      gas: "auto"
+    },
+    juDev: {
+      url: "http://47.236.98.58:8545",
+      accounts: process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : [],
+      gasPrice: 1100000000
+    },
+    juTest: {
+      url: "https://testnet-rpc.juchain.org",
+      accounts: process.env.PRIVATE_KEYS ? process.env.PRIVATE_KEYS.split(',') : [],
+      gasPrice: 1100000000
+    },
   },
 };
